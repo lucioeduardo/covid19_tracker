5143248c0117f6bcba0ba7d26bda471c64ddd14d
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2020-04-07 16:44:37 -0300

    Remaming file

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,4 +1,4 @@
-import 'package:corona_data/app/modules/home/widgets/brazil/brazil_page.dart';
+import 'package:corona_data/app/modules/home/widgets/brazil/brazil_widget.dart';
 import 'package:corona_data/app/modules/home/widgets/states_map/states_map_widget.dart';
 import 'package:corona_data/app/modules/home/widgets/world/world_widget.dart';
 import 'package:flutter/material.dart';
