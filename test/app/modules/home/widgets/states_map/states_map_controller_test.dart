d4cd81846e673855b738c7322c677a623564e7ef
Eduardo Lúcio <eduardo.lucio.correia@gmail.com>
2020-04-08 18:40:19 -0300

    #7 - StatesMapController Unit Tests

--- test/app/modules/home/widgets/states_map/states_map_controller_test.dart
+++ test/app/modules/home/widgets/states_map/states_map_controller_test.dart
@@ -1,25 +1,42 @@
+import 'package:corona_data/app/app_module.dart';
+import 'package:corona_data/app/modules/home/repositories/covid_repository_interface.dart';
+import 'package:corona_data/app/modules/home/widgets/states_map/states_map_controller.dart';
+import 'package:corona_data/app/shared/models/state_model.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:corona_data/app/modules/home/home_module.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../../covid_repository_mock.dart';
+import 'package:mockito/mockito.dart';
 
 void main() {
-  initModule(HomeModule());
-  // StatesMapController statesmap;
-  //
+  initModule(AppModule());
+
+  final CovidRepositoryMock covidRepositoryMock = CovidRepositoryMock();
+
+  when(covidRepositoryMock.getStatesInfo()).thenAnswer((_) => Future.value([
+        StateModel(confirmed: 15, deaths: 3, state: 'AL'),
+        StateModel(confirmed: 50, deaths: 20, state: 'PE'),
+      ]));
+
+  initModule(HomeModule(), changeBinds: [
+    Bind<ICovidRepository>((i) => covidRepositoryMock),
+  ]);
+
+  StatesMapController statesmap;
+
   setUp(() {
-    //     statesmap = HomeModule.to.get<StatesMapController>();
+    statesmap = Modular.get();
   });
 
   group('StatesMapController Test', () {
-    //   test("First Test", () {
-    //     expect(statesmap, isInstanceOf<StatesMapController>());
-    //   });
-
-    //   test("Set Value", () {
-    //     expect(statesmap.value, equals(0));
-    //     statesmap.increment();
-    //     expect(statesmap.value, equals(1));
-    //   });
+      test("Getting markers", () async{
+        await statesmap.markers;
+        expect(statesmap.markers.value.length, 2);
+        expect(statesmap.markers.value, isInstanceOf<Set<Marker>>());
+      });
   });
 }
