255808b3f3d77c3891526eafb86b94c65a467672
allan <luciocorreia.allan@gmail.com>
2020-04-17 15:17:36 -0300

    Countries Autocomplete Select working

--- lib/app/modules/settings/settings_controller.g.dart
+++ lib/app/modules/settings/settings_controller.g.dart
@@ -26,9 +26,52 @@ mixin _$SettingsController on _SettingsControllerBase, Store {
     }, _$themeAtom, name: '${_$themeAtom.name}_set');
   }
 
+  final _$formErrorsAtom = Atom(name: '_SettingsControllerBase.formErrors');
+
+  @override
+  ObservableMap<String, String> get formErrors {
+    _$formErrorsAtom.context.enforceReadPolicy(_$formErrorsAtom);
+    _$formErrorsAtom.reportObserved();
+    return super.formErrors;
+  }
+
+  @override
+  set formErrors(ObservableMap<String, String> value) {
+    _$formErrorsAtom.context.conditionallyRunInAction(() {
+      super.formErrors = value;
+      _$formErrorsAtom.reportChanged();
+    }, _$formErrorsAtom, name: '${_$formErrorsAtom.name}_set');
+  }
+
+  final _$_SettingsControllerBaseActionController =
+      ActionController(name: '_SettingsControllerBase');
+
+  @override
+  void addError(String field, String msg) {
+    final _$actionInfo =
+        _$_SettingsControllerBaseActionController.startAction();
+    try {
+      return super.addError(field, msg);
+    } finally {
+      _$_SettingsControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void cleanError(String field) {
+    final _$actionInfo =
+        _$_SettingsControllerBaseActionController.startAction();
+    try {
+      return super.cleanError(field);
+    } finally {
+      _$_SettingsControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
-    final string = 'theme: ${theme.toString()}';
+    final string =
+        'theme: ${theme.toString()},formErrors: ${formErrors.toString()}';
     return '{$string}';
   }
 }
