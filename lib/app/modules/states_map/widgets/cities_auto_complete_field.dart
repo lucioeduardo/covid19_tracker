2a2271b6c4e3e855a9a4c7172675adabc9ae6078
allan <luciocorreia.allan@gmail.com>
2020-05-15 11:54:23 -0300

    Refactor Cities Autocomplete and improve Ui and uix

--- lib/app/modules/states_map/widgets/cities_auto_complete_field.dart
+++ lib/app/modules/states_map/widgets/cities_auto_complete_field.dart
@@ -31,14 +31,23 @@ class _CitiesAutoCompleteFieldState extends State<CitiesAutoCompleteField> {
   final TextEditingController _typeAheadController = TextEditingController();
   _CitiesAutoCompleteFieldState();
   ExtraPallete extraPallete;
-  
+
   @override
   Widget build(BuildContext context) {
-    extraPallete=widget.globalSettingsController.theme.extraPallete;
+    extraPallete = widget.globalSettingsController.theme.extraPallete;
     return Container(
-      
-      margin: EdgeInsets.only(top: 20, left: 40, right: 40),
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),color: extraPallete.light,),
+      margin: EdgeInsets.only(top: 8, left: 10, right: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: extraPallete.light.withOpacity(0.7),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 4,
+              color: extraPallete.dark.withOpacity(0.1),
+              offset: Offset.fromDirection(1,4),
+              spreadRadius: 2
+            )
+          ]),
       child: TypeAheadField(
         hideSuggestionsOnKeyboardHide: true,
         debounceDuration: Duration(milliseconds: 500),
@@ -48,71 +57,89 @@ class _CitiesAutoCompleteFieldState extends State<CitiesAutoCompleteField> {
           autofocus: false,
           focusNode: widget.focusNode,
           style: TextStyle(
-              fontStyle: FontStyle.normal,
-              // backgroundColor: extraPallete.dark,
-              // decorationColor: extraPallete.dark,
-              
-              color: extraPallete.dark),
-          decoration: InputDecoration(
-              prefixStyle: TextStyle(fontSize: 16),
-              prefix: Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: FaIcon(
-                      
-                      FontAwesomeIcons.search,
-                      size:16,
-                      color: extraPallete.dark,
-                    ),
-              ),
-              suffixIcon: IconButton(
-                  iconSize: 16,
-                  padding: EdgeInsets.all(0),
-                  onPressed: () {
-                    _typeAheadController.clear();
-                  },
-                  icon: FaIcon(
-                    FontAwesomeIcons.times,
-                    color: extraPallete.dark,
-                  )),
-              border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(4.0)),gapPadding: 0.0),
-              labelText: "Tap for search.".i18n,
-              labelStyle: TextStyle(backgroundColor: Colors.transparent,fontWeight: FontWeight.w800, color: extraPallete.dark,)),
+            fontStyle: FontStyle.normal,
+            color: extraPallete.dark,
+          ),
+          decoration: inputDecoration(),
         ),
         suggestionsCallback: (pattern) async {
           return await widget.statesMapController.findMarkers(pattern);
         },
-        itemBuilder: (context, IMarkerModelData markerModel) {
-          return MarkersListTile(
-            widget: widget,
-            markerModel: markerModel,
-          );
-        },
-        onSuggestionSelected: (IMarkerModelData markerModel) {
-          _typeAheadController.clear();
-          widget.onSelected(markerModel);
-        },
+        itemBuilder: itemBuilder,
+        onSuggestionSelected: onSuggestionSelected,
+        noItemsFoundBuilder: noItemsFoundBuilder,
         loadingBuilder: (context) {
           return Text("Loading..".i18n);
-        },
-        noItemsFoundBuilder: (_) {
-          return Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              _typeAheadController.text.isNotEmpty
-                  ? "No itens found.".i18n
-                  : "Type for search.".i18n,
-              style: TextStyle(
-                color: Theme.of(context).accentColor,
-                fontWeight: FontWeight.w500,
-                fontSize: 15,
-              ),
-            ),
-          );
         },
         errorBuilder: (_, __) {
           return Text("blank".i18n);
         },
       ),
     );
+  }
+
+  void onSuggestionSelected(IMarkerModelData markerModel) {
+    _typeAheadController.clear();
+    widget.onSelected(markerModel);
+  }
+
+  Widget itemBuilder(context, IMarkerModelData markerModel) {
+    return MarkersListTile(
+      widget: widget,
+      markerModel: markerModel,
+    );
+  }
+
+  Widget noItemsFoundBuilder(context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Text(
+        _typeAheadController.text.isNotEmpty
+            ? "No itens found.".i18n
+            : "Type for search.".i18n,
+        style: TextStyle(
+          color: Theme.of(context).accentColor,
+          fontWeight: FontWeight.w500,
+          fontSize: 15,
+        ),
+      ),
+    );
+  }
+
+  InputDecoration inputDecoration() {
+    return InputDecoration(
+        // prefixStyle: TextStyle(fontSize: 16),
+        prefix: Padding(
+          padding: const EdgeInsets.only(right: 8.0),
+          child: FaIcon(
+            FontAwesomeIcons.search,
+            size: 16,
+            color: extraPallete.dark,
+          ),
+        ),
+        suffixIcon: IconButton(
+            iconSize: 16,
+            padding: EdgeInsets.all(0),
+            onPressed: () {
+              _typeAheadController.clear();
+            },
+            icon: FaIcon(
+              FontAwesomeIcons.times,
+              color: extraPallete.dark,
+            )),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderSide: BorderSide.none,
+          gapPadding: 0.0,
+        ),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(width: 2.0)),
+        labelText: "Tap for search.".i18n,
+        labelStyle: TextStyle(
+          backgroundColor: Colors.transparent,
+          fontWeight: FontWeight.w900,
+          color: extraPallete.dark,
+        ));
   }
 }
