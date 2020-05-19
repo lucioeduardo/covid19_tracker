b9c27fdc8e142878dc1c79b3e1c23a92f1f4533b
allan <luciocorreia.allan@gmail.com>
2020-05-19 12:52:38 -0300

    fixed adjustments

--- lib/app/shared/config/config.dart
+++ lib/app/shared/config/config.dart
@@ -2,7 +2,8 @@ import 'package:corona_data/app/shared/utils/localization/constants.dart';
 import 'package:corona_data/app/shared/utils/theme/constants.dart';
 import 'package:corona_data/app/shared/utils/localization/i18n/base_translation.i18n.dart';
 
-class Configuration {
+
+class Configuration{
   static const splashLoadingDuration = Duration(milliseconds: 2500);
   static const initialRoute = "/home";
   
@@ -11,7 +12,11 @@ class Configuration {
   static const defaultLocaleKey=kPTBR;
   static final locales = kLocales;
   static final baseTranslations = kBaseTranslations;
+
   
 
   const Configuration._();
+
+  
 }
+
